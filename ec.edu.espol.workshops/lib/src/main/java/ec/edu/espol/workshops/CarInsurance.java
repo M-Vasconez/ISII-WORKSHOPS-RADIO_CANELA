0b9be38bb3bb433e3d58b65ef5452f759cb65fca
Matias Vasconez <matvguer@espol.edu.ec>
2022-10-13 18:33:21 -0500

    Creation class CarInsurance

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -2,4 +2,12 @@ package ec.edu.espol.workshops;
 
 public class CarInsurance {
 
+	private final Integer base = 500;
+	private final Integer maleNotMarried25 = 1500;
+	private final Integer femMarried = 200;
+	private final Integer between45and65 = 100;
+	
+	private Integer age;
+	private String sex;
+	private String maritalStatus;
 }
