ce7ff8b29d95a975be5b63416d7133316f186281
oscaresm98 <osedsanc@espol.edu.ec>
2022-11-04 22:02:17 -0500

    test

---
+++ ec.edu.espol.workshops/lib/src/test/java/package/StepsDefs.java
@@ -0,0 +1,20 @@
+package ec.edu.espol.workshops;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.cucumber.javajava.en.Given;
+public class StepsDefs {
+	
+	@then("we should receive =1")
+	public void we_calculated_premium() {
+		int expectedPremium = 300;
+		CarInsurance carins1 = new CarInsurance(50, "f", "married", "2026/10/19");
+		assertEquals(expectedPremium,carins1.calculateInsurance());
+	}
+	
+	@then("we should receive =1")
+	public void we_calculated_premium() {
+		int expectedPremium = 500;
+		CarInsurance carins2 = new CarInsurance(28, "m", "notmarried", "2023/07/09");
+		assertEquals(expectedPremium,carins2.calculateInsurance());
+	}
+}
