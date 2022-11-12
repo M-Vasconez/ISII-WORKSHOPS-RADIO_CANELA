fb361d8ece1855dfa38eb8af7a4025e47aefb008
Matias Vasconez <matvguer@espol.edu.ec>
2022-11-11 23:10:01 -0500

    Se configuro los test

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/Test_CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/Test_CarInsurance.java
@@ -1,7 +1,7 @@
 package ec.edu.espol.workshops;
 
-import static org.junit.jupiter.api.Assertions.*;
-//import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 import org.junit.jupiter.api.Test;
 
@@ -10,30 +10,32 @@ class Test_CarInsurance {
 
 	@Test
 	void test1CalculateInsurance() {
-		int expectedPremium = 200;
-		CarInsurance carins1 = new CarInsurance(50, "f", "married", "2026/10/19");
-		assertEquals(expectedPremium,carins1.calculateInsurance());
+		int expectedPremium = -1;
+		int carins1 = ec.edu.espol.workshops.CarInsurance.validateInputs("10", "10", "10", "10");
+		assertEquals(expectedPremium,carins1);
 	}
 	
 	@Test
 	void test2CalculateInsurance() {
-		int expectedPremium = 500;
-		CarInsurance carins2 = new CarInsurance(28, "m", "notmarried", "2023/07/09");
-		assertEquals(expectedPremium,carins2.calculateInsurance());
+		int expectedPremium = -1;
+		int carins2 = ec.edu.espol.workshops.CarInsurance.validateInputs("90", "test", "test", "2022-10-05");
+		assertEquals(expectedPremium,carins2);
 	}
 	
 	@Test
 	void test3CalculateInsurance() {
-		boolean expectedPremium = false;
-		CarInsurance carins3 = new CarInsurance(88, "m", "married", "2025/10/04");
-		assertEquals(expectedPremium,carins3.calculateInsurance());
+		int expectedPremium = 0;
+		int carins3 = ec.edu.espol.workshops.CarInsurance.validateInputs("28", "m", "notmarried", "2026/10/05");
+		assertEquals(expectedPremium,carins3);
 	}
 	
 	@Test
 	void test4CalculateInsurance() {
-		boolean expectedPremium = false;
-		CarInsurance carins4 = new CarInsurance(25, "f", "notmarried", "2017/01/04");
-		assertEquals(expectedPremium,carins4.calculateInsurance());
+		int expectedPremium = -1;
+		int carins4 = ec.edu.espol.workshops.CarInsurance.validateInputs("veintiocho", "f", "married", "2026/10/05");
+		assertEquals(expectedPremium,carins4);
 	}
+	
+	
 
 }
